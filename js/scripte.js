d31844945655360b660d3cdc883e8681e4ef4b97
khalid <khalidcourses2022@gmail.com>
2023-10-03 15:06:22 +0100

    complete the home page by adding the footer and some animations.

--- js/scripte.js
+++ js/scripte.js
@@ -3,10 +3,31 @@
 
 var navLinks = document.getElementById("navLinks");
 
-function showMenu() {
-    navLinks.style.right = "0px";
+// function showMenu() {
+//     navLinks.style.right = "0px";
+// }
+
+// function hideMenu() {
+//     navLinks.style.right = "-200px";
+// }
+
+// just for test and now it works perfectly
+
+let menuBtn = document.querySelector(".fa-bars");
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('fa-times');
+    navLinks.classList.toggle('active');
 }
 
-function hideMenu() {
-    navLinks.style.right = "-200px";
-}
+// it worked but keep it just for later
+
+// window.addEventListener('scroll', function() {
+//     var header = document.getElementById('main-header');
+//     if (window.scrollY > 50) { // 50px is the threshold, change as needed
+//         header.classList.add('scrolled');
+//     } else {
+//         header.classList.remove('scrolled');
+//     }
+// });
+
