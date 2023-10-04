6a6366d804568696bf8ad1bb24dc7114d54bcc7a
khalid <khalidcourses2022@gmail.com>
2023-10-04 16:25:00 +0100

    adding the contact us page and its style + some improvments.

---
+++ form-handler.php
@@ -0,0 +1,62 @@
+<?php
+
+// $name = $_POST['name'];
+// $visitor_email = $_POST['email'];
+// $subject = $_POST['subject'];
+// $message = $_POST['message'];
+
+// // the email of the website that he will send you the email to the company 
+
+// $email_from = 'baytherm@website.com';
+
+// $email_subject = 'New Form Submission';
+
+// $email_body = "User Name : $name.\n". 
+//                 "User Email : $visitor_email./n". 
+//                 "Subject : $subject.\n". 
+//                 "User Message : $message .\n";
+
+// $to = 'khalidwebsites2023@gmail.com';
+
+// $headers = "From : $email_from \r\n";
+
+// $headers .= "Reply-To : $visitor_email /r/n";
+
+// mail($to , $email_subject , $email_body , $headers);
+
+// header("Location : contact.html");
+
+// improved by chatgbt
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+    $visitor_email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
+    $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_STRING);
+    $message = filter_input(INPUT_POST, 'message', FILTER_SANITIZE_STRING);
+
+    $email_from = 'baytherm@website.com';
+
+    $email_subject = 'New Form Submission';
+
+    $email_body = "User Name: $name.\n".
+                  "User Email: $visitor_email.\n".
+                  "Subject: $subject.\n".
+                  "User Message: $message.\n";
+
+    $to = 'khalidwebsites2023@gmail.com';
+
+    $headers = "From: $email_from \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
+
+    if(mail($to, $email_subject, $email_body, $headers)) {
+        header("Location: contact.html");
+    } else {
+        echo "Mail sending failed.";
+    }
+}
+
+
+
+?>
